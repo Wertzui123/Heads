910e843a7b1dbb39ffe1182042e9bd5989be4a33
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-05-07 21:41:41 +0200

    Update HeadEntity.php

--- src/Enes5519/PlayerHead/entities/HeadEntity.php
+++ src/Enes5519/PlayerHead/entities/HeadEntity.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Forked from:
  *
@@ -21,11 +20,8 @@
  *
  * Copyright (C) 2019 Wertzui123
  */
-
 declare(strict_types=1);
-
 namespace Enes5519\PlayerHead\entities;
-
 use Enes5519\PlayerHead\PlayerHead;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\BlockIds;
@@ -38,55 +34,43 @@ use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\Player;
 use pocketmine\utils\UUID;
 use pocketmine\utils\TextFormat;
-
 class HeadEntity extends Human
 {
-
     public const HEAD_GEOMETRY = '{"geometry.player_head":{"texturewidth":64,"textureheight":64,"bones":[{"name":"head","pivot":[0,24,0],"cubes":[{"origin":[-4,0,-4],"size":[8,8,8],"uv":[0,0]}]}]}}';
-
     public $width = 0.5, $height = 0.6;
-
     protected function initEntity(): void
     {
         $this->setMaxHealth(1);
         parent::initEntity();
         $this->setSkin(new Skin($this->skin->getSkinId(), $this->skin->getSkinData(), "", "geometry.player_head", self::HEAD_GEOMETRY));
     }
-
     public function hasMovementUpdate(): bool
     {
         return false;
     }
-
     public function getUniqueId(): UUID
     {
         return $this->uuid;
     }
-
     public function attack(EntityDamageEvent $source): void
     {
         if ($source instanceof EntityDamageByEntityEvent and $source->getDamager() instanceof Player and $source->getCause() === EntityDamageEvent::CAUSE_ENTITY_ATTACK) {
             $player = $source->getDamager();
-
             if ($player->hasPermission("cb-heads.kill")) {
                 $pos = $this->asPosition();
                 $pos->x = $pos->getFloorX();
                 $pos->y = $pos->getFloorY();
                 $pos->z = $pos->getFloorZ();
                 $block = BlockFactory::get(BlockIds::SKULL_BLOCK, 0, $pos);
-
                 ($event = new BlockBreakEvent($player, $block, $player->getInventory()->getItemInHand(), false, $this->getDrops()))->call();
-
                 if(!$event->isCancelled()){
                     parent::attack($source);
                 }
             }
         }
     }
-
     public function getDrops(): array
     {
         return [PlayerHead::getPlayerHeadItem($this->getSkin())];
     }
-
 }
